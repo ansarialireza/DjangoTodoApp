9aeb55801a70c4a7b2fc91e92ae589a37b3e7661
alireza <58270983+ansarialireza@users.noreply.github.com>
2024-08-14 14:41:13 +0330

    ✅ update todo app

--- todo/urls.py
+++ todo/urls.py
@@ -5,4 +5,7 @@ app_name = 'todo'
 
 urlpatterns = [
     path('', TaskListView.as_view(), name='task-list'),
+    path("task/add/", TaskCreateView.as_view(), name="task-add"),
+    path("task/<int:pk>/", TaskUpdateView.as_view(), name="task-update"),
+    path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,36 @@
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Task
+from .forms import TaskForm
 
-class TaskListView(ListView):
+
+class TaskListView(LoginRequiredMixin,ListView):
     model = Task
     context_object_name = 'tasks'
     ordering = ['created_at']
+    
+    # def get_queryset(self):
+    #     return Task.objects.filter(user = self.request.user).order_by('created_at')
+    
+class TaskCreateView(CreateView):
+    model = Task
+    form_class = TaskForm
+    fields = ["name"]
+    
+    # def form_valid(self, form):
+    #     comment = form.save(commit=False)
+    #     comment.post = self.object
+    #     comment.save()
+    #     messages.success(self.request, 'ثبت با موفقیت انجام شد')
+    #     return redirect(self.request.path_info)
+
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    form_class = TaskForm
+    fields = ["name"]
+
+
+class TaskDeleteView(DeleteView):
+    model = Task
