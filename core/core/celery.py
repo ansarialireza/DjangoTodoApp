eba19e4462ee39fd0a8483373b974a9f51429df8
alireza <58270983+ansarialireza@users.noreply.github.com>
2024-11-01 20:58:51 +0330

    ✅

--- core/core/celery.py
+++ core/core/celery.py
@@ -15,8 +15,8 @@ app = Celery('core')
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
-app.config_from_object(f'django.conf:{settings.__name__}', namespace='CELERY')
-# app.config_from_object('django.conf:settings', namespace='CELERY')
+# app.config_from_object(f'django.conf:{settings.__name__}', namespace='CELERY')
+app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
